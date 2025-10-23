44c87ea03b12707c88faf3d7217245aaf818031c
Martin <martin@1696.io>
2025-10-23 19:15:45 -0400

    FOR-1 | Remove content on front page

--- front/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders ForgeCMS link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/ForgeCMS/i);
-  expect(linkElement).toBeInTheDocument();
-});
